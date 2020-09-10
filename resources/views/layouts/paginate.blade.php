dcd209cad2360b557813f13eb7a5ecf7b53b3f11
Ryunosuke Yamada <yryunosuke012@gmail.com>
2020-09-10 19:00:42 +0900

    ページネーションのデザイン変更と1ページだけでも表示されるよう修正

--- resources/views/layouts/paginate.blade.php
+++ resources/views/layouts/paginate.blade.php
@@ -1,3 +1,11 @@
 <div class="mt-5 d-flex justify-content-center">
-    {{ $clips->links() }}
+    @if ($clips->hasPages())
+        {{ $clips->links('pagination.default') }}
+    @else
+        <div class="g_pager">
+            <a class="prev"></a>
+            <a class="current" href="">1</a>
+            <a class="next"></a>
+        </div>
+    @endif
 </div>
