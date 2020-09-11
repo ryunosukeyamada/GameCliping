cb1ad43035548622a61f9b422b3a21f98cf5f009
Ryunosuke Yamada <yryunosuke012@gmail.com>
2020-09-11 14:27:59 +0900

    usersテーブルにcaptionカラムを追加しました ここに簡単な自己紹介が入ります

---
+++ database/migrations/2020_09_11_141318_add_caption_to_users_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddCaptionToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('caption','100')->default('Hello World');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+}
