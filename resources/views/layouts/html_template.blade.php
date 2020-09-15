a34ae7218e123b61d415fc14fb5ba83dd614f4ed
Ryunosuke Yamada <yryunosuke012@gmail.com>
2020-09-15 20:59:27 +0900

    ツイートするときにカードが表示されるようになりました。

--- resources/views/layouts/html_template.blade.php
+++ resources/views/layouts/html_template.blade.php
@@ -6,10 +6,24 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
     <!-- ファビコン -->
-    <link rel="icon" href="{{ asset('img/favicon.ico') }}">
+    <link rel="icon" href="{{ secure_asset('img/favicon.ico') }}">
     <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+
+    <meta name="twitter:card" content="summary_large_image" />
+    <!--①-->
+    <meta name="twitter:site" content="@ProgramRy" />
+    <!--②-->
+    <meta property="og:url" content="https://game-cliping.herokuapp.com/" />
+    <!--③-->
+    <meta property="og:title" content="Game Cliping" />
+    <!--④-->
+    <meta property="og:description" content="あなたのお気に入りの動画共有しませんか？" />
+    <!--⑤-->
+    <meta property="og:image" content="{{ secure_asset('img/gamecliping.jpg') }}" />
+    <!--⑥-->
+
     @yield('head')
-    
+
     <!-- Font Awesome -->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <!-- Google Fonts -->
@@ -24,7 +38,7 @@
     <div id="app">
         @yield('content')
     </div>
-    
+
     <script src="{{ mix('js/app.js') }}"></script>
     <!-- JQuery -->
     <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
